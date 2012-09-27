b97090c0ff53c4d5c0924171cae27b3268ec7721
Rein Raudjärv <rein@zeroturnaround.com>
2012-09-27 10:42:41 +0300

    Fixed BarrierExecutor.

--- counter/src/main/java/org/zeroturnaround/jf2012/concurrency/counter/BarrierExecutor.java
+++ counter/src/main/java/org/zeroturnaround/jf2012/concurrency/counter/BarrierExecutor.java
@@ -4,12 +4,14 @@ import java.util.concurrent.CyclicBarrier;
 
 public class BarrierExecutor extends CreateAndStartExecutor {
 
+  private static final int CYCLE_AFTER_EACH = 1000;
+  
   @Override
   public void invoke(Runnable task, int threads, int times) throws InterruptedException {
     // Wrap the task such that it will synchronize between all parties
     CyclicBarrier barrier = new CyclicBarrier(threads);
-    BarrierAction action = new BarrierAction(task, barrier);
-    super.invoke(action, threads, times);
+    Runnable cycle = new BarrierAction(new Repeat(task, CYCLE_AFTER_EACH), barrier);
+    super.invoke(cycle, threads, times / CYCLE_AFTER_EACH);
   }
 
 }
