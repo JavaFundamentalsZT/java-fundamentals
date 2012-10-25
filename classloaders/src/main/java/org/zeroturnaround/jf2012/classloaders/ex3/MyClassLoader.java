0d4672c73d01e8fc70e6a308abdf798ea45ef489
Toomas Romer <toomas@zeroturnaround.com>
2012-10-25 11:33:55 +0300

    Simplified first example

--- classloaders/src/main/java/org/zeroturnaround/jf2012/classloaders/ex3/MyClassLoader.java
+++ classloaders/src/main/java/org/zeroturnaround/jf2012/classloaders/ex3/MyClassLoader.java
@@ -1,10 +1,17 @@
 package org.zeroturnaround.jf2012.classloaders.ex3;
 
-public class MyClassLoader extends ClassLoader {
+import java.net.URL;
+import java.net.URLClassLoader;
+
+public class MyClassLoader extends URLClassLoader {
+  public MyClassLoader(URL[] urls, ClassLoader cl) {
+    super(urls, cl);
+  }
+
   @Override
   public Class<?> loadClass(String name) throws ClassNotFoundException {
     System.out.println("loadClass " + name);
-    return getParent().loadClass(name);
+    return super.loadClass(name);
   }
   
   @Override
--- classloaders/src/main/java/org/zeroturnaround/jf2012/classloaders/ex3/MyClassLoaderMain.java
+++ classloaders/src/main/java/org/zeroturnaround/jf2012/classloaders/ex3/MyClassLoaderMain.java
@@ -1,18 +1,20 @@
 package org.zeroturnaround.jf2012.classloaders.ex3;
 
 import java.lang.reflect.Method;
+import java.net.URL;
 
 public class MyClassLoaderMain {
 
   public static void main(String[] args) throws Exception {
-    MyClassLoader myClassLoader = new MyClassLoader();
+    URL myClasses = new URL("file:/Users/toomasr/projects/java-fundamentals-course/classloaders/custom/classes/");
+    MyClassLoader myClassLoader = new MyClassLoader(new URL[] { myClasses }, ClassLoader.getSystemClassLoader());
+    
     Class myClass = myClassLoader.loadClass("org.zeroturnaround.jf2012.classloaders.ex3.MyCustomClass");
 
     Object myObj = myClass.newInstance();
     Method m = myObj.getClass().getMethod("printSomething", null);
     m.invoke(myObj, null);
-    
+
     System.out.println(myObj);
   }
-
 }
