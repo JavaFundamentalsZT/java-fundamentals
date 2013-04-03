e939ada2ff9730b31caec4bb8fd5457d697e0742
Toomas Romer <toomas@zeroturnaround.com>
2013-04-03 15:57:59 +0300

    Updating examples

--- classloaders/src/main/java/org/zeroturnaround/jf2012/classloaders/ex1/SimpleClass.java
+++ classloaders/src/main/java/org/zeroturnaround/jf2012/classloaders/ex1/SimpleClass.java
@@ -1,6 +1,5 @@
 package org.zeroturnaround.jf2012.classloaders.ex1;
 
-
 public class SimpleClass {
   public static void main(String[] args) {
     System.out.println("Tom is cool!");
--- classloaders/src/main/java/org/zeroturnaround/jf2012/classloaders/ex2/Example.java
+++ classloaders/src/main/java/org/zeroturnaround/jf2012/classloaders/ex2/Example.java
@@ -3,13 +3,14 @@ package org.zeroturnaround.jf2012.classloaders.ex2;
 public class Example {
   public static void main(String[] args) {
     ClassLoader cl = Example.class.getClassLoader();
-    System.out.println(cl);
-    
-    //System.out.println(cl.getParent());
-    //System.out.println(cl.getClass().getClassLoader());
-    
-    //System.out.println(System.getProperty("java.ext.dirs"));
-    //System.out.println(com.sun.nio.zipfs.ZipInfo.class.getClassLoader());
-    //System.out.println(cl.getParent().getParent());
+    // System.out.println(cl);
+    // System.out.println(ClassLoader.getSystemClassLoader());
+
+    // System.out.println(cl.getParent());
+    // System.out.println(cl.getClass().getClassLoader());
+
+    // System.out.println(System.getProperty("java.ext.dirs"));
+    // System.out.println(com.sun.nio.zipfs.ZipInfo.class.getClassLoader());
+    // System.out.println(cl.getParent().getParent());
   }
 }
