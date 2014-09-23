c75a6877f0ff905c1c1ca28d16d83f6af802365c
Neeme Praks <neeme.praks@zeroturnaround.com>
2014-09-23 09:45:59 +0300

    removed type

--- net/src/main/java/datagram/TestPayloadSize.java
+++ net/src/main/java/datagram/TestPayloadSize.java
@@ -8,25 +8,22 @@ import java.nio.ByteBuffer;
 public class TestPayloadSize {
 
   public static void main(String[] args) throws Exception {
-    testByteBuffer('I', Long.MAX_VALUE);
-    testObjectOutputStream('I', Long.MAX_VALUE);
+    testByteBuffer(Long.MAX_VALUE);
+    testObjectOutputStream(Long.MAX_VALUE);
   }
 
-  private static void testByteBuffer(char type, long value) {
+  private static void testByteBuffer(long value) {
     ByteBuffer buf = ByteBuffer.allocate(48);
-    buf.put((byte)type);
     buf.putLong(value);
     buf.flip();
     System.out.println("result buf: " + buf.remaining() + " bytes");
     ByteBuffer buf2 = ByteBuffer.wrap(buf.array());
-    System.out.println("char equals: " + (buf2.get() == type));
     System.out.println("long equals: " + (buf2.getLong() == value));
   }
 
-  private static void testObjectOutputStream(char type, long value) throws IOException {
+  private static void testObjectOutputStream(long value) throws IOException {
     ByteArrayOutputStream bos = new ByteArrayOutputStream();
     ObjectOutputStream oos = new ObjectOutputStream(bos);
-    oos.writeByte(type);
     oos.writeLong(value);
     oos.flush();
     System.out.println("result oos: " + bos.toByteArray().length + " bytes");
