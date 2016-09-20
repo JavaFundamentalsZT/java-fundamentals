bd156b88fec448de9e5aeb9d9cc5afbfe72b506f
Rein Raudjärv <rein@zeroturnaround.com>
2016-09-20 11:05:20 +0300

    Added some IO examples.

---
+++ io/src/main/java/Turkish.java
@@ -0,0 +1,23 @@
+import java.util.Locale;
+
+/**
+ * Created by rein on 14/09/16.
+ */
+public class Turkish {
+
+  public static void main(String[] args) {
+    test("i", Locale.ENGLISH);
+    test("I", Locale.ENGLISH);
+//    Locale turkey = new Locale("tr-TR");
+    Locale turkey = new Locale("tr", "TR");
+    test("i", turkey);
+    test("I", turkey);
+  }
+
+  private static void test(String input, Locale locale) {
+    String upper = input.toUpperCase(locale);
+    String lower = input.toLowerCase(locale);
+    System.out.format("%s\tUPPER: %s (%b)\tlower: %s (%b)\t%s%n", input, upper, upper.equals(input), lower, lower.equals(input), locale);
+  }
+
+}
---
+++ io/src/main/java/print/FormatExample.java
@@ -0,0 +1,18 @@
+package print;
+
+/**
+ * Created by rein on 14/09/16.
+ */
+public class FormatExample {
+
+  public static void main(String[] args) {
+    int i = 2;
+    double r = Math.sqrt(i);
+    System.out.format("The square root of %d is %f.%n", i, r);
+
+    String key = "foo";
+    boolean value = true;
+    System.out.format("%s: %b%n", key, value);
+  }
+
+}
