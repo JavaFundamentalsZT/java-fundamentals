7e7754737f972d2e19b758dd3d9cf2814d379680
Toomas Romer <toomas@zeroturnaround.com>
2016-10-25 10:39:27 +0300

    Preset to clean demo

--- classloaders/src/main/java/org/zeroturnaround/jf/classloaders/ex2/Example.java
+++ classloaders/src/main/java/org/zeroturnaround/jf/classloaders/ex2/Example.java
@@ -2,18 +2,19 @@ package org.zeroturnaround.jf.classloaders.ex2;
 
 public class Example {
   public static void main(String[] args) {
-    ClassLoader cl = Example.class.getClassLoader();
-    System.out.println("Example class's classloader " + cl);
+    // ClassLoader cl = Example.class.getClassLoader();
+    // System.out.println("Example class's classloader " + cl);
 
     // System.out.println("Example class's classloader " + cl.getClass());
     // System.out.println("Classloader's classloader " + cl.getClass().getClassLoader());
+
     // System.out.println("Parent class loader " + cl.getParent());
-    // //
-    // // System.out.println(System.getProperty("java.ext.dirs"));
-    // System.out.println(com.sun.nio.zipfs.ZipInfo.class.getClassLoader());
-    //
     // System.out.println(cl.getParent().getParent());
     //
     // System.out.println("System classloader " + ClassLoader.getSystemClassLoader());
+    //
+    // System.out.println(System.getProperty("java.ext.dirs"));
+    // System.out.println(com.sun.nio.zipfs.ZipInfo.class.getClassLoader());
+    //
   }
 }
