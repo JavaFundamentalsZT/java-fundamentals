77ef725d7deadd05e3380d8216a89c753a811680
Toomas Romer <toomas@zeroturnaround.com>
2016-10-25 10:24:39 +0300

    Reset ex2 to a pre demo state

--- classloaders/src/main/java/org/zeroturnaround/jf/classloaders/ex2/Example.java
+++ classloaders/src/main/java/org/zeroturnaround/jf/classloaders/ex2/Example.java
@@ -5,14 +5,15 @@ public class Example {
     ClassLoader cl = Example.class.getClassLoader();
     System.out.println("Example class's classloader " + cl);
 
-    System.out.println("Classloader's classloader " + cl.getClass().getClassLoader());
-    System.out.println("Parent class loader " + cl.getParent());
-
-    System.out.println(System.getProperty("java.ext.dirs"));
-    System.out.println(com.sun.nio.zipfs.ZipInfo.class.getClassLoader());
-    
-    System.out.println(cl.getParent().getParent());
-
-    System.out.println("System classloader " + ClassLoader.getSystemClassLoader());
+    // System.out.println("Example class's classloader " + cl.getClass());
+    // System.out.println("Classloader's classloader " + cl.getClass().getClassLoader());
+    // System.out.println("Parent class loader " + cl.getParent());
+    // //
+    // // System.out.println(System.getProperty("java.ext.dirs"));
+    // System.out.println(com.sun.nio.zipfs.ZipInfo.class.getClassLoader());
+    //
+    // System.out.println(cl.getParent().getParent());
+    //
+    // System.out.println("System classloader " + ClassLoader.getSystemClassLoader());
   }
 }
