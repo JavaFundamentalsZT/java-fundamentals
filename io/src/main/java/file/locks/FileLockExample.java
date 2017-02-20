d775399a241ac5e580ec2f7acfe924b7bb123fe1
Tõnis Pool <tonis.pool@zeroturnaround.com>
2017-02-20 10:32:23 +0200

    Changed FileLockExample to actually lock a temp file.

--- io/src/main/java/file/locks/FileLockExample.java
+++ io/src/main/java/file/locks/FileLockExample.java
@@ -4,10 +4,12 @@ import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.channels.FileLock;
+import java.nio.file.Files;
 
 public class FileLockExample {
   public static void main(String[] args) throws IOException {
-    File lockFile = null; // some file to lock
+    File lockFile = Files.createTempFile("lock", "txt").toFile(); // some file to lock
+    lockFile.deleteOnExit();
     try (RandomAccessFile raf = new RandomAccessFile(lockFile, "rw");
          FileLock lock = raf.getChannel().lock()) { // #lock blocks!
       System.out.println("Locked: " + lockFile + " with " + lock);
